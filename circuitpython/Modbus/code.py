1fd59438700174908206e7e389d0250c022edee1
HWHardsoft <info@hwhardsoft.de>
2021-10-26 12:00:53 +0200

    Add files via upload

--- circuitpython/Modbus/code.py
+++ circuitpython/Modbus/code.py
@@ -6,6 +6,8 @@
 # see the Pycom Licence v1.0 document supplied with this file, or
 # available at https://www.pycom.io/opensource/licensing
 #
+# changed for Feather Industrial Controller by Zihatec GmbH
+#
 
 import board
 import busio
@@ -15,8 +17,7 @@ from uModBusSerial import uModBusSerial
 
 
 ######################### RTU SERIAL MODBUS #########################
-modbus_obj = uModBusSerial(baudrate=115200)
-
+modbus_obj = uModBusSerial(baudrate=9600)
 
 while True:
 
@@ -29,7 +30,7 @@ while True:
     #print('Coil status: ' + ' '.join('{:d}'.format(x) for x in coil_status))
 
 
-    ###################### READ INPUT REGISTERS ##################
+    # ##################### READ INPUT REGISTERS ##################
     slave_addr=0x01
     starting_address=0x10
     register_quantity=10
@@ -39,12 +40,59 @@ while True:
     print('Input register value: ' + ' '.join('{:d}'.format(x) for x in register_value))
 
 
-    # ##################### WRITE SINGLE REGISTER ##################
-    #slave_addr = 0x01
-    #register_address = 18
-    #register_value = 0
-    #signed = True
+    # ##################### READ DISCRETE INPUTS ##################
+    #slave_addr=0x0A
+    #starting_address=0x0
+    #input_quantity=100
 
-    #return_flag = modbus_obj.write_single_register(slave_addr, register_address, register_value, signed)
+    #input_status = modbus_obj.read_discrete_inputs(slave_addr, starting_address, input_quantity)
+    #print('Input status: ' + ' '.join('{:d}'.format(x) for x in input_status))
+
+
+    # ##################### READ HOLDING REGISTERS ##################
+    #slave_addr=0x0A
+    #starting_address=0x00
+    #register_quantity=100
+    #signed=True
+
+
+    # ##################### WRITE SINGLE REGISTER ##################
+    slave_addr = 0x01
+    register_address = 18
+    register_value = 0
+    signed = True
+
+    return_flag = modbus_obj.write_single_register(slave_addr, register_address, register_value, signed)
+    output_flag = 'Success' if return_flag else 'Failure'
+    print('Writing single coil status: ' + output_flag)
+
+
+    # ##################### WRITE MULIPLE COILS ##################
+    #slave_addr=0x0A
+    #starting_address=0x00
+    #output_values=[1,1,1,0,0,1,1,1,0,0,1,1,1]
+
+    #return_flag = modbus_obj.write_multiple_coils(slave_addr, starting_address, output_values)
+    #output_flag = 'Success' if return_flag else 'Failure'
+    #print('Writing multiple coil status: ' + output_flag)
+
+
+    # ##################### WRITE SINGLE COIL ##################
+    #slave_addr=0x0A
+    #output_address=0x00
+    #output_value=0xFF00
+
+    #return_flag = modbus_obj.write_single_coil(slave_addr, output_address, output_value)
     #output_flag = 'Success' if return_flag else 'Failure'
     #print('Writing single coil status: ' + output_flag)
+
+
+    # ##################### WRITE MULIPLE COILS ##################
+    #slave_addr=0x0A
+    #starting_address=0x00
+    #output_values=[1,1,1,0,0,1,1,1,0,0,1,1,1]
+
+    #return_flag = modbus_obj.write_multiple_coils(slave_addr, starting_address, output_values)
+    #output_flag = 'Success' if return_flag else 'Failure'
+    #print('Writing multiple coil status: ' + output_flag)
+
